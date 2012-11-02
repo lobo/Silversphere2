7f8f920a18a977ef8afd56bc4ccfd66bf21eea15
Daniel Lobo <daniel_lobo_182@hotmail.com>
2012-11-02 20:00:00 -0300

    listo

--- src/backend/Floor.java
+++ src/backend/Floor.java
@@ -47,7 +47,6 @@ public class Floor extends Cell implements ContentOperations, Serializable {
 
 	@Override
 	public boolean isAccesible() {
-
 		return content != null;
 	}
 
---
+++ src/backend/SaveandLoadGame.java
@@ -0,0 +1,52 @@
+package backend;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import javax.swing.JOptionPane;
+
+public class SaveandLoadGame {
+
+	public void SaveGame(Board board, String file)
+			throws FileNotFoundException, IOException {
+
+		try {
+			ObjectOutputStream newFile = new ObjectOutputStream(
+					new BufferedOutputStream(new FileOutputStream(file)));
+			newFile.writeObject(board);
+			newFile.close();
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Unable To Save");
+			e.printStackTrace();
+		}
+
+	}
+
+	public Board LoadGame(String file) throws FileNotFoundException,
+			IOException, ClassNotFoundException {
+		try {
+			ObjectInputStream newfile = new ObjectInputStream(
+					new BufferedInputStream(new FileInputStream(file)));
+			Board game = (Board) newfile.readObject();
+			game.getBoard().setInstanceBoardListener(
+					new InstanceBoardListener());
+			return game;
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null,
+					"Unable to Load - Unacceptable File Type");
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			JOptionPane.showMessageDialog(null,
+					"Unable to Load - Unacceptable File Type");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+}

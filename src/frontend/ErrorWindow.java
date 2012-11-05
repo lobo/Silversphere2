0566af93a2cd75c960fd70b82d1e7b0c09be53b3
Daniel Lobo <daniel_lobo_182@hotmail.com>
2012-11-05 19:10:49 -0300

    modified shit in file

--- src/frontend/ErrorWindow.java
+++ src/frontend/ErrorWindow.java
@@ -9,46 +9,31 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-<<<<<<< HEAD
 public class ErrorWindow extends JDialog {
 
 	public ErrorWindow(String str, final JFrame frame) {
-=======
-public class ErrorWindow extends JDialog{
 
-	public ErrorWindow(String str, final JFrame frame){
-		
->>>>>>> 0fbf34c02731d7c621354735dfc9178d6c78ddbf
 		frame.setEnabled(false);
 		setAlwaysOnTop(true);
 		setLocationRelativeTo(null);
 		setResizable(false);
-<<<<<<< HEAD
+
 		setSize(250, 100);
 		centerScreen();
 		setTitle("ERROR");
 
 		JLabel label = new JLabel(str, JLabel.CENTER);
+
 		add(label);
+
 		addWindowListener(new WindowAdapter() {
 			@Override
-=======
-		setSize(250,100);
-		centerScreen();
-		setTitle("ERROR");
-        
-		JLabel label = new JLabel(str, JLabel.CENTER);
-	
-		add(label);
-		
-		addWindowListener(new WindowAdapter() {
->>>>>>> 0fbf34c02731d7c621354735dfc9178d6c78ddbf
 			public void windowClosing(WindowEvent e) {
 				setVisible(false);
 				frame.setEnabled(true);
 			}
 		});
-<<<<<<< HEAD
+
 	}
 
 	public void centerScreen() {
@@ -56,16 +41,6 @@ public class ErrorWindow extends JDialog{
 		Dimension size = toolkit.getScreenSize();
 		setLocation(size.width / 2 - getWidth() / 2, size.height / 2
 				- getHeight() / 2);
-=======
-		
-	}
-
-	public void centerScreen(){
-		Toolkit toolkit = getToolkit();
-        Dimension size = toolkit.getScreenSize();
-        setLocation(size.width/2 - getWidth()/2,
-        size.height/2 - getHeight()/2);
->>>>>>> 0fbf34c02731d7c621354735dfc9178d6c78ddbf
 	}
 
 }

70e909cf0aad82bfd115e531181e0450640d13a3
Felipe Martinez <fmart318@hotmail.com>
2012-11-05 19:11:57 -0300

    saveAndLoadGame

--- src/backend/SaveandLoadGame.java
+++ src/backend/SaveandLoadGame.java
@@ -1,54 +1,68 @@
 package backend;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.BufferedOutputStream;
 
 import javax.swing.JOptionPane;
 
-public class SaveandLoadGame {
+//import frontend.InstanceBoardListener; faltaria este metodo en el front
 
-	public void SaveGame(Board board, String file)
-			throws FileNotFoundException, IOException {
+public class SaveandLoadGame {
+	
+	public SaveandLoadGame() {
+	}
+	
+	/** Save game in new file
+	 * 
+	 * @param board Board to save
+	 * @param file name of the file
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
+
+		public void SaveGame(Board board, String file) throws FileNotFoundException, IOException{
 
 		try {
-			ObjectOutputStream newFile = new ObjectOutputStream(
-					new BufferedOutputStream(new FileOutputStream(file)));
-			newFile.writeObject(board);
+			ObjectOutputStream newFile = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream(file)));
+			newFile.writeObject(board);	
 			newFile.close();
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Unable To Save");
 			e.printStackTrace();
 		}
-
-	}
-
-	public Board LoadGame(String file) throws FileNotFoundException,
-			IOException, ClassNotFoundException {
-		try {
-			ObjectInputStream newfile = new ObjectInputStream(
-					new BufferedInputStream(new FileInputStream(file)));
-			Board game = (Board) newfile.readObject();
-			/*
-			 * ENTRE COMMENTS PORQUE NO COMPILA
-			 * game.getBoard().setInstanceBoardListener( new
-			 * InstanceBoardListener()); return game;
-			 */
-		} catch (IOException e) {
-			JOptionPane.showMessageDialog(null,
-					"Unable to Load - Unacceptable File Type");
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			JOptionPane.showMessageDialog(null,
-					"Unable to Load - Unacceptable File Type");
-			e.printStackTrace();
+		
 		}
-		return null;
-	}
-
+		
+		/** Create a game with saved information
+		 * @see SaveGame
+		 * 
+		 * @param file name of the file
+		 * @return Board in the file.
+		 * @throws FileNotFoundException
+		 * @throws IOException
+		 * @throws ClassNotFoundException
+		 */
+		
+		public Board LoadGame (String file) throws FileNotFoundException, IOException, ClassNotFoundException{
+			try {
+				ObjectInputStream newfile = new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)));
+				Board board = (Board)newfile.readObject();
+				//board.setInstanceBoardListener(new InstanceBoardListener()); el  mensaje instanceBoardListener tendria q estar en el front
+				return board;				
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, "Unable to Load - Unacceptable File Type");
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				JOptionPane.showMessageDialog(null, "Unable to Load - Unacceptable File Type");
+				e.printStackTrace();
+			}
+			return null;
+		}
+		
 }

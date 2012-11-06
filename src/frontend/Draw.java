6bd3fe845913de30220436f9174b822a9b205c2d
Daniel Lobo <daniel_lobo_182@hotmail.com>
2012-11-06 01:21:43 -0300

    borre los printfs

--- src/frontend/Draw.java
+++ src/frontend/Draw.java
@@ -57,13 +57,12 @@ public class Draw {
 				return loadImage(images.get(cell.getClass()));
 			} else {
 				// fijar back x getContent en Cell
-				 if (cell.getContent().getClass().equals(Player.class)) {
-				 if (!playerPresence) {
-				 System.out.println("entro en draw");
-				 playerPresence = true;
-				 return loadImage(images.get(cell.getClass()));
-				 }
-				 }
+				if (cell.getContent().getClass().equals(Player.class)) {
+					if (!playerPresence) {
+						playerPresence = true;
+						return loadImage(images.get(cell.getClass()));
+					}
+				}
 				return loadImage(images.get(cell.getContent().getClass()));
 			}
 		}
